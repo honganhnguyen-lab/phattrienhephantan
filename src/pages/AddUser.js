5f066b523d487a66fa6e9e690e8fafa436413bd5
MacOs <nhonganh49@gmail.com>
2021-10-26 18:44:36 +0700

    change setState value

--- src/pages/AddUser.js
+++ src/pages/AddUser.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { TextField } from '@mui/material';
+import { ClassNames } from '@emotion/react';
+
+const useStyles = makeStyles(theme=>({
+    root: {
+ 
+            width: '75ch',
+        
+    },
+}));
 
 const AddUser = () => {
+    const classes = useStyles();
+    const [state,setState] = useState({
+        name:'',
+        email:'',
+        contact:'',
+        address:'',
+    });
+    const {name,email,contact,address} = state;
+
     return (
         <div>
-            <h2>Add User</h2>
+            <form className= {classes.root} noValidate autoComplete="off">
+                <TextField  id= "standard-basic" label= "Standard" />
+            </form>
         </div>
     )
 }
